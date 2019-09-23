7486c19a376b265a337d435f36b13ab87770bbc4
Eloi Baulenas <eloibaucid24@gmail.com>
2019-09-23 20:42:28 +0200

    Exercici Senzill

---
+++ Exercicis senzills/Exercici.java
@@ -0,0 +1,16 @@
+public class Exercici {
+    public static void main(String[] args) {
+        String Nom;
+        String Cognom;
+        String Domini;
+
+        System.out.println("Introdueix el teu cognom");
+        Nom = System.console().readLine();
+        System.out.println("Introdueix el teu cognom");
+        Cognom = System.console().readLine();
+        System.out.println("Introdueix el teu cognom");
+        Domini = System.console().readLine();
+        
+        System.out.println("La teva adreça de correu és:" +Nom+.+Cognom+"@"+Domini);
+    }
+}
