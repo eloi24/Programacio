57d046bdc08550a0e406cc471a48c4eaae37453b
Eloi Baulenas <eloibaucid24@gmail.com>
2019-09-25 20:00:01 +0200

    Exercici 4

---
+++ Exercicis senzills/Exercici4.java
@@ -0,0 +1,23 @@
+public class Exercici4{
+    public static void main(String[] args) {
+        String article;
+        float Preu;
+        float descompte;
+        float preufinal;
+
+        System.out.println("Introdueix el nom de l'article:");
+        article = System.console().readLine();
+        System.out.println("Introdueix el preu original:");
+        Preu = Float.parseFloat(System.console().readLine());
+        System.out.println("Introdueix el descompte a aplicar (%)");
+        descompte = Float.parseFloat(System.console().readLine()) ;
+
+preufinal = Preu-((Preu*15)/100);
+System.out.println("El preu final de "+article+" és "+preufinal );
+
+
+
+
+
+    }
+}
