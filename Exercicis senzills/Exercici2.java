79f290f2c9b4b5a808c9059f44a6adbd11ae45c1
Eloi Baulenas <eloibaucid24@gmail.com>
2019-09-25 18:56:09 +0200

    Exercici 2

--- Exercicis senzills/Exercici2.java
+++ Exercicis senzills/Exercici2.java
@@ -5,10 +5,25 @@ public class Exercici2 {
     float imc;
       System.out.println("Introdueix la teva alçada en centímetres: ");
       altura = Float.parseFloat(System.console().readLine());
+      altura = (altura/100);
       System.out.println("Introdueix el teu pes en kg: ");
       pes = Float.parseFloat(System.console().readLine());
-      altura = (altura/100)
-      imc = pes/Math.pow(altura, 2);
-      System.out.println(+imc);
-    }
+      imc = pes/(altura*altura);
+
+      if (imc< 18.5) {
+        System.out.println("Has de menjar més el teu imc és "+imc+" per tant és inferior a 18.5 i has de menjar mes");
+      }
+      else if (imc >=25){
+        System.out.println("Estàs gras, hauries de fer dieta el teu imc és: "+imc);
+      }
+      else if (imc >= 18.5) {
+        System.out.println("Estas en el pes ideal el teu imc és: "+imc);
+      }
+      
+      else{
+        System.out.println("El valor no és correcte");
+      }
+      
+    
+  }
 }
