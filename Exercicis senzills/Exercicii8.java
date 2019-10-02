be75313c55fed38af185933c77b598ce0c8ffc56
eloi24 <eloibaucid24@gmail.com>
2019-10-02 18:08:31 +0200

    Exercici8

--- Exercicis senzills/Exercicii8.java
+++ Exercicis senzills/Exercicii8.java
@@ -2,15 +2,19 @@ import java.util.Random;
 public class Exercicii8{
     public static void main(String[] args) {
       Random aleatori= new Random();
-      int premi = aleatori.nextInt(100);
-      int i;
+      int premi = aleatori.nextInt(10);
       int intents = 0;
 boolean trobat = false;
 System.out.println("El número escollit és: "+premi);
-int arraynums [] = new int [100];
+int arraynums [] = new int [10];
+int i;
+
+for (i=0; i<arraynums.length;i++){
+    arraynums[i]=2000;
+}
 
 while(!trobat){
-int aleatori1 = aleatori.nextInt(100);
+int aleatori1 = aleatori.nextInt(10);
 boolean igual = false;
 
 for (i=0;i<arraynums.length;i++){
@@ -27,10 +31,7 @@ if (!igual){
 if (premi==aleatori1){
     trobat=true;
 }
-else {
-    System.out.println("el número "+i+" ja hi era");  
 }
-}
-System.out.println("Hem necessitat "+intents+" intents");
+System.out.println("Has necessitat "+intents+" intents");
     }
 }
